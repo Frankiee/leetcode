e88fe05a52ca74b788f20953153377b36248ddbb
Weixin Liu <frank@postmates.com>
2019-02-02 15:23:15 -0800

    more

--- trie/677_map_sum_pairs.py
+++ trie/677_map_sum_pairs.py
@@ -2,7 +2,7 @@
 # 677. Map Sum Pairs
 
 # Implement a MapSum class with insert, and sum methods.
-
+#
 # For the method insert, you'll be given a pair of (string, integer).
 # The string represents the key and the integer represents the value.
 # If the key already existed, then the original key-value pair will be
@@ -11,7 +11,7 @@
 # For the method sum, you'll be given a string representing the prefix,
 # and you need to return the sum of all the pairs' value whose key starts
 # with the prefix.
-
+#
 # Example 1:
 # Input: insert("apple", 3), Output: Null
 # Input: sum("ap"), Output: 3
