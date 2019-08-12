b0151ac83c53ee1626f502bfc79aadf4597b5ab6
Weixin Liu <frank@postmates.com>
2019-08-11 20:45:31 -0700

    131. Palindrome Partitioning

--- backtracking/131_palindrome_partitioning.py
+++ backtracking/131_palindrome_partitioning.py
@@ -20,24 +20,22 @@ class Solution(object):
     def is_palindrome(self, s):
         return s == s[::-1]
 
-    def dfs(self, s, start, ret, temp):
-        for i in range(start + 1, len(s) + 1):
-            if self.is_palindrome(s[start:i]):
-                new_temp = temp[:]
-                new_temp.append(s[start:i])
-                if i == len(s):
-                    ret.append(new_temp)
-                else:
-                    self.dfs(s, i, ret, new_temp)
+    def dfs(self, ret, s, start, prefix):
+        if start == len(s):
+            ret.append(prefix)
+        else:
+            for i in range(start + 1, len(s) + 1):
+                new_word = s[start:i]
+                if self.is_palindrome(new_word):
+                    self.dfs(ret, s, i, prefix + [new_word])
 
     def partition(self, s):
         """
         :type s: str
         :rtype: List[List[str]]
         """
-        if not s:
-            return [[]]
-
         ret = []
-        self.dfs(s, 0, ret, [])
+
+        self.dfs(ret, s, 0, [])
+
         return ret
---
+++ backtracking/README.md
@@ -0,0 +1,3 @@
+A general approach to backtracking questions in Java (Subsets, Permutations, Combination Sum, Palindrome Partioning)
+
+https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
