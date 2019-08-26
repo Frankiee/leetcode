ca6e5977f159e7ed2030543fad02134f016c818f
Weixin Liu <frank@postmates.com>
2019-08-26 02:06:17 -0700

    297. Serialize and Deserialize Binary Tree

--- graph_tree_dfs/serialization_deserialization/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ graph_tree_dfs/serialization_deserialization/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -1,4 +1,4 @@
-# [Import, Tree-Serialization-Deserialization]
+# [Tree-Serialization-Deserialization]
 # https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 # 105. Construct Binary Tree from Preorder and Inorder Traversal
 
